7b4e0fcf2f13d6ad71012b8cddf2cd0281137def
mstewart284 <mstewart@sandscap.com>
2026-07-15 09:37:35 -0400

    Update GS_BASKET_PRICES SQL query for specific fields
    Refine query to select specific columns and filter by date.

--- models_sandbox/gs_basket_prices.sql
+++ models_sandbox/gs_basket_prices.sql
@@ -3,6 +3,10 @@
 ) }}
 
 select
-    *
-from SC_RISK.MARQUEE_DATA.GS_BASKET_PRICES
-limit 100;
+    basket_symbol,
+    price_date,
+    price,
+    update_time,
+    load_ts
+from {{ source('marquee_data', 'GS_BASKET_PRICES') }}
+where price_date >= dateadd(day, -90, current_date)
